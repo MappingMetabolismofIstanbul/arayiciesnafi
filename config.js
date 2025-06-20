ca4316abeb33fb012a825d4529f842dc4ac63683
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-06-20 10:38:49 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,78 @@
+var config = {
+    style: 'mapbox://styles/dogatmn/cm4v9fem6000p01sf12f769y5/draft',
+    accessToken: 'pk.eyJ1IjoiZG9nYXRtbiIsImEiOiJjbHlyMTJmcXgwMjQ3MmtzbTRsdm1pejdsIn0.vcEjL7lt8OoOPMP5o1PM6g',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'light',
+    use3dTerrain: false,
+    auto: false,
+    title: 'Waste and Sanitation',
+    subtitle: '',
+    byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
+    chapters: [
+        {
+            id: 'first',
+            alignment: 'centered',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.96451771366735 , 41.00314331950173],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+           {
+            id: 'second',
+            alignment: 'centered',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.96451771366735 , 41.00314331950173],
+                zoom: 13,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third',
+            alignment: 'centered',
+            hidden: false,
+            title: '',
+            image: 'https://i.ibb.co/5xhhgrMG/MEF-AAP-027-BAP-2025-06-20-07-20-04.png',
+            description: '',
+            location: {
+                center: [28.96451771366735 , 41.00314331950173],
+                zoom: 16,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        
+    ]
+};
