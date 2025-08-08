880a0625294dc93f787f8426044ffd8e69d761df
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-09 00:46:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false,
     auto: false,
-    title: 'Arayıcı Esnafı / Street Cleaner',
+    title: 'Arayıcı Esnafı / Scavenger',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
